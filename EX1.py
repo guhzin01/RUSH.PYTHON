57fdc2df004ae2ad5bbfe0d8edcb576e445abae8
guhzin081 <guhzindev@gmail.com>
2026-03-23 02:25:15 -0300

    DAY1

---
+++ EX1.py
@@ -0,0 +1,9 @@
+valor1 = int(input("DIGITE um numero: "))
+valor2 = int(input("DIGITE um numero: "))
+
+
+if valor1 > valor2:
+    print("O valor 1 e maior")
+elif valor2 > valor1:
+    print("valor 2 e maior")
+
